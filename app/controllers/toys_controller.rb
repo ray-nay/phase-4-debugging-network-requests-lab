c020168a30e9be621f5048ee63ae6403289d7bae
Renee Mumbi <1723renee@gmail.com>
2023-03-19 16:21:08 +0300

    Done

--- app/controllers/toys_controller.rb
+++ app/controllers/toys_controller.rb
@@ -7,13 +7,15 @@ class ToysController < ApplicationController
   end
 
   def create
-    toy = Toys.create(toy_params)
+    toy = Toy.create(toy_params)
     render json: toy, status: :created
+    
   end
 
   def update
     toy = Toy.find_by(id: params[:id])
     toy.update(toy_params)
+    render json: toy
   end
 
   def destroy
@@ -21,11 +23,20 @@ class ToysController < ApplicationController
     toy.destroy
     head :no_content
   end
+#   def increment_likes
+#   toy = Toy.find_by(id: params[:id])
+#   if toy
+#     toy.update(likes: toy.likes + 1)
+#     render json: toy
+#   else
+#     render json: { error: "Toy not found" }, status: :not_found
+#   end
+# end
 
   private
   
   def toy_params
-    params.permit(:name, :image, :likes)
+    params.permit(:id, :name, :image, :likes)
   end
 
 end
